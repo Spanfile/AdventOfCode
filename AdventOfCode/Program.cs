77ff05231af0fd9473d79f141d13ff8585925c49
Spanfile <santeri.kasvi@gmail.com>
2017-12-04 18:22:49 +0200

    Project + day 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,7 +2,7 @@
 
 namespace AdventOfCode
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
---
+++ Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Day1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new Program().SolvePart2();
+            Console.ReadKey();
+        }
+
+        private Program()
+        {
+        }
+
+        private void SolvePart1()
+        {
+            var input = LoadInput();
+
+            var captcha = input.Select((c, i) => (int.Parse(c.ToString()), int.Parse(input[(i + 1) % input.Length].ToString()))).Where(pair => pair.Item1 == pair.Item2).Sum(pair => pair.Item1);
+            Console.WriteLine(captcha);
+        }
+
+        private void SolvePart2()
+        {
+            var input = LoadInput();
+
+            var captcha = input.Select((c, i) => (int.Parse(c.ToString()), int.Parse(input[(i + input.Length / 2) % input.Length].ToString()))).Where(pair => pair.Item1 == pair.Item2).Sum(pair => pair.Item1);
+            Console.WriteLine(captcha);
+        }
+
+        private string LoadInput() => File.ReadAllText("input.txt");
+        private string SampleInput1() => "1122";
+        private string SampleInput2() => "1111";
+        private string SampleInput3() => "1234";
+        private string SampleInput4() => "91212129";
+        private string SampleInput5() => "1212";
+        private string SampleInput6() => "1221";
+        private string SampleInput7() => "123425";
+        private string SampleInput8() => "123123";
+        private string SampleInput9() => "12131415";
+    }
+}
