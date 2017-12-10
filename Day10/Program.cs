cbcb92bf057ad218b63a876650326416ae7b42c7
Spanfile <santeri.kasvi@gmail.com>
2017-12-10 15:25:29 +0200

    Rename all projects to be sorted in order

---
+++ Day10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new Program().SolvePart1();
+            Console.ReadLine();
+        }
+
+        private Program()
+        {
+        }
+
+        private void SolvePart1()
+        {
+
+        }
+    }
+}
