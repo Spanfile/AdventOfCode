3c4f3ac9a575e3d24f8ab64512e721d2c03ae2cb
Spanfile <santeri.kasvi@gmail.com>
2017-12-25 16:39:52 +0200

    Day 24

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Day24
 {
@@ -23,7 +20,52 @@ namespace Day24
 
         private void SolvePart1()
         {
-            var input = SampleInput1();
+            var input = LoadInput().Select(l =>
+            {
+                var args = l.Split('/');
+                return (int.Parse(args[0]), int.Parse(args[1]));
+            });
+
+            var components = new Dictionary<int, List<int>>();
+
+            foreach (var component in input)
+            {
+                if (components.ContainsKey(component.Item1))
+                    components[component.Item1].Add(component.Item2);
+                else
+                    components.Add(component.Item1, new List<int> {component.Item2});
+                
+                if (components.ContainsKey(component.Item2))
+                    components[component.Item2].Add(component.Item1);
+                else
+                    components.Add(component.Item2, new List<int> {component.Item1});
+            }
+
+            var bridgeInfos = BuildBridges(components)
+                .Select(bridge => (bridge.Count, bridge.Sum(component => component.Item1 + component.Item2)))
+                .ToArray();
+            var strongest = bridgeInfos.OrderByDescending(i => i.Item2).First().Item2;
+            var longest = bridgeInfos.OrderByDescending(i => i.Item1).First().Item2;
+            Console.WriteLine($"Strongest: {strongest}, longest and strongest: {longest}");
+        }
+
+        private IEnumerable<List<(int, int)>> BuildBridges(Dictionary<int, List<int>> components,
+            List<(int, int)> bridge = null)
+        {
+            if (bridge == null)
+                bridge = new List<(int, int)> {(0, 0)};
+
+            var current = bridge[bridge.Count - 1].Item2;
+            foreach (var b in components[current])
+            {
+                if (bridge.Contains((current, b)) || bridge.Contains((b, current)))
+                    continue;
+                
+                var newBridge = new List<(int, int)>(bridge).Append((current, b)).ToList();
+                yield return newBridge;
+                foreach (var other in BuildBridges(components, newBridge))
+                    yield return other;
+            }
         }
 
         private string[] LoadInput() => File.ReadAllLines("input.txt");
